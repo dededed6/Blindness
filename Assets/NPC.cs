d25b8e4eacaced0daeeeafd0a4609a4562df5585
dededed6 <dededed338@gmail.com>
2019-04-07 20:32:12 +0900

    first commit

---
+++ Assets/NPC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject target = Player.player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform tranform = target.transform;
+        this.transform.LookAt(tranform);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 ScreenCenter;
+    public static GameObject player;
+    public float speed = 2f;
+    public float sen = 3f;
+    public Camera cam;
+    void Start()
+    {
+        cam = Camera.allCameras[0];
+        ScreenCenter = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2);
+        player =  this.gameObject;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(ScreenCenter);
+        Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("a");
+            GameObject objectHit = hit.collider.gameObject;
+            //if(objectHit.tag == "Player")
+            //{
+                Destroy(objectHit);
+            //}
+        }
+        
+        Move();
+    }
+    void Move()
+    {
+        this.transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime);
+        float rotX = Input.GetAxis("Mouse Y") * sen;
+        float rotY = Input.GetAxis("Mouse X") * sen;
+
+        this.transform.localRotation *= Quaternion.Euler(0, rotY, 0);
+        cam.transform.localRotation *= Quaternion.Euler(-rotX, 0, 0);
+    }
+}
